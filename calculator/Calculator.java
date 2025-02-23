c168dcadf601b5e67bd033c1a1bf4acdf762de3b
Camacho <camachocastelanjm@gmail.com>
2025-02-22 23:24:14 -0600

    Add files via upload

---
+++ calculator/Calculator.java
@@ -0,0 +1,19 @@
+package mx.uv.fiee.iinf.poo.calculator;
+
+public class Calculator<T extends Number> {
+    private T[] nums;
+
+    public Calculator (T[] nums) {
+        this.nums = nums; // El constructor guarda el arreglo de 5 elementos en nums
+    }
+    public double average(){
+        // Metodo que calcula el promedio del arreglo
+        double sum = 0;
+        // Suma de todos los numeros
+        for(int i = 0; i<5; i++){
+            sum+=nums[i].doubleValue(); // Conversion de cualquier tipo numerico a tipo double
+        }
+        // Retorno del valor promedio
+        return sum/5;
+    }
+}
